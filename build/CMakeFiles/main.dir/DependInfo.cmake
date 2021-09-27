30e95dcf2b8d3399459a6d5c6c05048433e6d9db
jstzwjr <wangjianrongtz@163.com>
2021-09-27 16:31:32 +0800

    first commit

---
+++ build/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/workspace_wjr/develop/tutorials/ncnn_test/04_retinaface/FaceDetector.cpp" "/workspace_wjr/develop/tutorials/ncnn_test/04_retinaface/build/CMakeFiles/main.dir/FaceDetector.cpp.o"
+  "/workspace_wjr/develop/tutorials/ncnn_test/04_retinaface/main.cpp" "/workspace_wjr/develop/tutorials/ncnn_test/04_retinaface/build/CMakeFiles/main.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/workspace_wjr/3rd/opencv/include"
+  "/workspace_wjr/3rd/opencv/include/opencv"
+  "/workspace_wjr/3rd/ncnn/build/install/include/ncnn"
+  "/workspace_wjr/3rd/1.2.189.0/x86_64/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
